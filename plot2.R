039bed05f706ecb210214b55f40c26b70876fda3
Cornel Bicuti <cornel.bicuti@gmail.com>
2014-05-11 23:42:57 +0100

    Formatting

--- plot2.R
+++ plot2.R
@@ -8,10 +8,11 @@ filtered_data <- subset(power_data, Date == "1/2/2007" | Date == "2/2/2007")
 png(filename = "plot2.png", width = 480, height = 480, bg = "white")
 
 filtered_data$Global_active_power <- sapply(filtered_data$Global_active_power, as.numeric)
+filtered_data$Date_Time <- strptime(paste(filtered_data$Date, filtered_data$Time), format = "%d/%m/%Y %H:%M:%S")
 
-filtered_data$Date_Time <- strptime(paste(filtered_data$Date, filtered_data$Time), format="%d/%m/%Y %H:%M:%S")
-
-plot(filtered_data$Date_Time,filtered_data$Global_active_power,
+plot(filtered_data$Date_Time, filtered_data$Global_active_power,
+     type = "l",
+     xlab = "",
      ylab = "Global Active Power (kilowatts)")
 	 
 dev.off()
